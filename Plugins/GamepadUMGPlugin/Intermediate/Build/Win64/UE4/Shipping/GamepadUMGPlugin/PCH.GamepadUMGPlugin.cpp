a795defa3b62fce3e5a2b909f30aed9c31b90e59
ΣRRΩR 404 <nathan.cm77@gmail.com>
2021-01-21 23:02:59 -0300

    Added gamepad navegation for UI

---
+++ Plugins/GamepadUMGPlugin/Intermediate/Build/Win64/UE4/Shipping/GamepadUMGPlugin/PCH.GamepadUMGPlugin.cpp
@@ -0,0 +1,1 @@
+#include "S:/natha/Documents/Unreal Project/FNAF/FiveNightAtFreddys/Plugins/GamepadUMGPlugin/Intermediate/Build/Win64/UE4/Shipping/GamepadUMGPlugin/PCH.GamepadUMGPlugin.h"
---
+++ Plugins/GamepadUMGPlugin/Intermediate/Build/Win64/UE4Editor/Development/GamepadUMGPlugin/PCH.GamepadUMGPlugin.cpp
@@ -0,0 +1,1 @@
+#include "S:/natha/Documents/Unreal Project/FNAF/FiveNightAtFreddys/Plugins/GamepadUMGPlugin/Intermediate/Build/Win64/UE4Editor/Development/GamepadUMGPlugin/PCH.GamepadUMGPlugin.h"
